6f5981e1d6e34848731d4f5a68383a9dac67903d
Benjamin <benjamin@trevald.com>
2020-08-23 23:33:23 +0200

    Added favicons

--- src/components/AppMyTodos.vue
+++ src/components/AppMyTodos.vue
@@ -1,63 +1,66 @@
 <template>
-	<div class="view-col">
-		<h1>My ToDos</h1>
-		<article v-for="document in documentsWithTodos" :key="document.id">
-			<h2>
-				<router-link :to="{ name: 'Document', params: { documentId: document.id } }">{{
+  <div class="view-col">
+    <h1>My ToDos</h1>
+    <article
+      v-for="document in documentsWithTodos"
+      :key="document.id"
+    >
+      <h2>
+        <router-link :to="{ name: 'Document', params: { documentId: document.id } }">{{
 					getDocumentName(document)
 				}}</router-link>
-			</h2>
-			<ul data-type="todo_list">
-				<AppTodoSingle
-					v-for="(todo, index) in getTodos(document)"
-					:key="index"
-					:todo="todo"
-					>{{ todo.textContent }}</AppTodoSingle
-				>
-			</ul>
-		</article>
-	</div>
+      </h2>
+
+      <ul data-type="todo_list">
+        <AppTodoSingle
+          v-for="(todo, index) in getTodos(document)"
+          :key="index"
+          :todo="todo"
+        >{{ todo.textContent }}</AppTodoSingle>
+      </ul>
+    </article>
+  </div>
 </template>
 
 <script>
-	import { documentGetName, documentGetTodos } from "./../utils/document"
-	import AppTodoSingle from "./AppTodoSingle"
+import { documentGetName, documentGetTodos } from "./../utils/document";
+import AppTodoSingle from "./AppTodoSingle";
 
-	export default {
-		name: "AppMyTodos",
+export default {
+  name: "AppMyTodos",
 
-		components: {
-			AppTodoSingle,
-		},
+  components: {
+    AppTodoSingle,
+  },
 
-		data() {
-			return {}
-		},
+  data() {
+    return {};
+  },
 
-		computed: {
-			documentsWithTodos() {
-				return this.$store.getters.documentsWithTodos
-			},
-		},
+  computed: {
+    documentsWithTodos() {
+      return this.$store.getters.documentsWithTodos;
+    },
+  },
 
-		methods: {
-			getDocumentName(doc) {
-				return documentGetName(doc)
-			},
+  methods: {
+    getDocumentName(doc) {
+      return documentGetName(doc);
+    },
 
-			getTodos(doc) {
-				return documentGetTodos(doc)
-			},
-		},
-	}
+    getTodos(doc) {
+      return documentGetTodos(doc);
+    },
+  },
+};
 </script>
 
 <style scoped>
-	h2 a {
-		text-decoration: none;
-	}
+h2 a {
+  text-decoration: none;
+}
 
-	h2 a:hover {
-		text-decoration: underline;
-	}
+h2 a:hover {
+  text-decoration: underline;
+}
 </style>
